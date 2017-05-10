441025b923bcb411cd0609395c089a4edfd08c8e
piaozaiguang <zaiguang.piao@gmail.com>
2017-05-10 11:37:28 +0800

    init module discovery.

---
+++ discovery-eureka/src/main/java/io/github/piaozaiguang/spring/cloud/discovery/EurekaApplication.java
@@ -0,0 +1,16 @@
+package io.github.piaozaiguang.spring.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by piaozaiguang on 2017/5/10.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
