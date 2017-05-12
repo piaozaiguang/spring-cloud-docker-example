93511a300e57959548b1a1948a0acd95c92c75d2
piaozaiguang <zaiguang.piao@gmail.com>
2017-05-12 17:05:52 +0800

    rename config-server

---
+++ config-server/src/main/java/io/github/piaozaiguang/spring/cloud/config/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package io.github.piaozaiguang.spring.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created by piaozaiguang on 2017/5/12.
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
